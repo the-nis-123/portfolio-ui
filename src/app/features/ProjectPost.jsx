681d9eb1c9cb5b53182873964c4685d4e83b0154
kintu denis <vienseydenis@gmail.com>
2023-01-27 17:44:48 +0300

    updated

--- src/app/features/ProjectPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectPost = () => {
-  return (
-    <div>ProjectPost</div>
-  )
-}
-
-export default ProjectPost
--- src/components/award card/Award.jsx
+++ src/components/award card/Award.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import {useEffect, useState} from 'react';
+import getFileStream from '../../app/api/getFileStream';
 
 const Award = ({data}) => {
   const [image, setImage] = useState();
